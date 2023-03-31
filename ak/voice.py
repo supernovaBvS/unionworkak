03123b00fc0658a8fa7fe5fd3dd5d14064edaff5
supernovaBvS <kongson14@gmail.com>
2023-03-31 16:42:46 +0800

    added speak function

--- ak/voice.py
+++ ak/voice.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+import pyttsx3
 import cv2
 import random
 import time
@@ -6,13 +7,13 @@ import numpy as np
 import pydobot
 from serial.tools import list_ports
 
-available_ports = list_ports.comports()
+# available_ports = list_ports.comports()
 # print(f'available ports: {[x.device for x in available_ports]}')
-port = available_ports[2].device
+# port = available_ports[2].device
 
-d = pydobot.Dobot(port)
-d.suck(False)
-d.speed()
+# d = pydobot.Dobot(port)
+# d.suck(False)
+# d.speed()
 
 z= -50
 x= 300
@@ -27,32 +28,47 @@ pos = [(x+2, y+y_offset*2-35),
         (x+2*x_offset, y+43)]
 print('start')
 
-# Create a recognizer object
 r = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
 # Use the default microphone as the audio source
-while True:
-    with sr.Microphone(1) as source:
+def listen():
+    with sr.Microphone() as source:
         # Adjust for ambient noise
         r.adjust_for_ambient_noise(source, duration=0.2)
         
         # Listen for user input
         print("Say something!")
+        speak("Say something!")
         audio = r.listen(source)
         # Recognize speech using Google Speech Recognition
-        text = r.recognize_google(audio)
-        print("You said: ", text)
-        break
+        try:
+            text = r.recognize_google(audio)
+            print("You said: ", text)
+            return text
+        except:
+            return None
 
-if 'coffee' in text:
-    hx, hy = pos[0]
-    d.jump(hx, hy, z+10,0)
-    print('this is your coffee')
-else:
-    hx, hy = pos[3]
-    d.jump(hx, hy, z+10,0)
-    print('this is your tea')
+while True:
+    command = listen()
 
-d.jump(250,0,70)
-print('end')
-d.close()
+    if command is None:
+        continue
+
+    if 'coffee' in command:
+        # hx, hy = pos[0]
+        # d.jump(hx, hy, z+10,0)
+        speak('this is your coffee')
+    else:
+        # hx, hy = pos[3]
+        # d.jump(hx, hy, z+10,0)
+        speak('this is your tea')
+
+    # d.jump(250,0,70)
+    print('end')
+    break
+    # d.close()
